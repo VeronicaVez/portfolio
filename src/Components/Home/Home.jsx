6a5bd5c3f53c677e3ec9cac02b0e77520a639773
Veronica Vezzoli <veronicavezzoli94@gmail.com>
2024-05-31 18:45:25 +0200

    try different home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,89 +4,94 @@ const Home = () => {
   return (
     <div className="Home" id="home">
       <div className="animation2">
-        <svg width="220" height="20">
+        <svg width="18%" height="10">
           <defs>
             <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset="1%" stop-color="#6A5495" stop-opacity="1" />
-              <stop offset="100%" stop-color="#ED5EDD" stop-opacity="1" />
+              <stop offset="1%" stopColor="#6A5495" />
+              <stop offset="100%" stopColor="#ED5EDD" />
             </linearGradient>
           </defs>
-          <rect x="10" y="10" width="200" height="10" fill="url(#grad1)" />
+          <rect x="10" y="0" width="100%" height="10" fill="url(#grad1)" />
         </svg>
-        <svg width="7.5" height="7.5">
-          <circle cx="2" cy="2" r="2" fill="white" />
+        <svg width="2%" height="10">
+          <circle cx="50%" cy="50%" r="1" fill="white" />
         </svg>
-        <svg width="160" height="20">
+        <svg width="12%" height="10">
           <defs>
             <linearGradient id="grad2" x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset="1%" stop-color="#F3CA52" stop-opacity="1" />
-              <stop offset="100%" stop-color="#7ABA78" stop-opacity="1" />
+              <stop offset="1%" stopColor="#F3CA52" />
+              <stop offset="100%" stopColor="#7ABA78" />
             </linearGradient>
           </defs>
-          <rect x="5" y="10" width="150" height="10" fill="url(#grad2)" />
+          <rect x="0" y="0" width="100%" height="10" fill="url(#grad2)" />
         </svg>
       </div>
+
       <div className="animation">
-        <svg width="270" height="20">
+        <svg width="22%" height="10">
           <defs>
             <linearGradient id="grad3" x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset="1%" stop-color="#FAA300" stop-opacity="1" />
-              <stop offset="100%" stop-color="#F4538A" stop-opacity="1" />
+              <stop offset="1%" stopColor="#FAA300" />
+              <stop offset="100%" stopColor="#F4538A" />
             </linearGradient>
           </defs>
-          <rect x="10" y="10" width="250" height="10" fill="url(#grad3)" />
+          <rect x="10" y="0" width="100%" height="10" fill="url(#grad3)" />
         </svg>
-        <svg width="7.5" height="7.5">
-          <circle cx="2" cy="2" r="2" fill="white" />
+        <svg width="2%" height="10">
+          <circle cx="50%" cy="50%" r="1" fill="white" />
         </svg>
-        <svg width="190" height="20">
+        <svg width="20%" height="10">
           <defs>
             <linearGradient id="grad4" x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset="1%" stop-color="#8BE8E5" stop-opacity="1" />
-              <stop offset="100%" stop-color="#A084E8" stop-opacity="1" />
+              <stop offset="1%" stopColor="#8BE8E5" />
+              <stop offset="100%" stopColor="#A084E8" />
             </linearGradient>
           </defs>
-          <rect x="10" y="10" width="180" height="10" fill="url(#grad4)" />
+          <rect x="0" y="0" width="100%" height="10" fill="url(#grad4)" />
         </svg>
       </div>
+
       <div className="animation2">
-        <svg width="120" height="20">
-          <rect x="10" y="10" width="100" height="10" fill="url(#grad2)" />
+        <svg width="12%" height="10">
+          <rect x="10" y="0" width="100%" height="10" fill="url(#grad2)" />
         </svg>
-        <svg width="7.5" height="7.5">
-          <circle cx="2" cy="2" r="2" fill="white" />
+        <svg width="2%" height="10">
+          <circle cx="50%" cy="50%" r="1" fill="white" />
         </svg>
-        <svg width="220" height="20">
-          <rect x="10" y="10" width="200" height="10" fill="url(#grad1)" />
+        <svg width="18%" height="10">
+          <rect x="0" y="0" width="100%" height="10" fill="url(#grad1)" />
         </svg>
-        <svg width="7.5" height="7.5">
-          <circle cx="2" cy="2" r="2" fill="white" />
+        <svg width="2%" height="10">
+          <circle cx="50%" cy="50%" r="1" fill="white" />
         </svg>
-        <svg width="260" height="20">
-          <rect x="10" y="10" width="250" height="10" fill="url(#grad3)" />
+        <svg width="20%" height="10">
+          <rect x="0" y="0" width="100%" height="10" fill="url(#grad3)" />
         </svg>
       </div>
+
       <div className="animation3">
-        <svg width="275" height="20">
-          <rect x="10" y="10" width="250" height="10" fill="url(#grad4)" />
+        <svg width="20%" height="10">
+          <rect x="10" y="0" width="100%" height="10" fill="url(#grad4)" />
         </svg>
-        <svg width="7.5" height="7.5">
-          <circle cx="2" cy="2" r="2" fill="white" />
+        <svg width="2%" height="10">
+          <circle cx="50%" cy="50%" r="1" fill="white" />
         </svg>
-        <svg width="220" height="20">
-          <rect x="10" y="10" width="200" height="10" fill="url(#grad2)" />
+        <svg width="22%" height="10">
+          <rect x="0" y="0" width="100%" height="10" fill="url(#grad2)" />
         </svg>
-        <svg width="7.5" height="7.5">
-          <circle cx="2" cy="2" r="2" fill="white" />
+        <svg width="2%" height="10">
+          <circle cx="50%" cy="50%" r="1" fill="white" />
         </svg>
-        <svg width="230" height="20">
-          <rect x="10" y="10" width="220" height="10" fill="url(#grad1)" />
+        <svg width="18%" height="10">
+          <rect x="0" y="0" width="100%" height="10" fill="url(#grad1)" />
         </svg>
       </div>
+
       <div className="animation">
         <h1>Veronica Vezzoli</h1>
         <h2>Web Developer</h2>
       </div>
+
       <div className="animation2">
         <svg width="270" height="20">
           <rect x="10" y="10" width="250" height="10" fill="url(#grad3)" />
@@ -104,6 +109,7 @@ const Home = () => {
           <rect x="5" y="10" width="150" height="10" fill="url(#grad2)" />
         </svg>
       </div>
+
       <div className="animation">
         <svg width="170" height="20">
           <rect x="10" y="10" width="150" height="10" fill="url(#grad4)" />
@@ -132,6 +138,7 @@ const Home = () => {
           <rect x="10" y="10" width="200" height="10" fill="url(#grad4)" />
         </svg>
       </div>
+
       <div className="animation3">
         <svg width="270" height="20">
           <rect x="10" y="10" width="250" height="10" fill="url(#grad2)" />
